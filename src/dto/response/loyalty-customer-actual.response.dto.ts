cf761c92544a03a17e93ba225009ed0c87e69e64
Sugeng Winanjuar <winanjuar@gmail.com>
2023-01-13 16:27:06 +0700

    standarisasi swagger message

--- src/dto/response/loyalty-customer-actual.response.dto.ts
+++ src/dto/response/loyalty-customer-actual.response.dto.ts
@@ -16,8 +16,7 @@ export class LoyaltyCustomerActualResponseDto extends BaseResponseDto {
   statusCode: number;
 
   @ApiProperty({
-    example:
-      'Get data customer with ID 67746a2b-d693-47e1-99f5-f44572aee307 successfully',
+    example: 'This is sample get single data successfully',
   })
   message: string;
 
